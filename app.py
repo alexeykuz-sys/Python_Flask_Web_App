8bc835db7f6451c5ab5872d158890b555e882250
unknown <alexey kuzmis>
2021-03-04 15:47:53 +0000

    'add'

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
     return "dfdfgfghg"
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
