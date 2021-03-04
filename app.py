88bb71f6c95eb3d3953f4d4cb709db2d22f22e6e
alexeykuz-sys <alexeykuz@gmail.com>
2021-03-04 21:43:11 +0000

    add home and about page

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 if os.path.exists("env.py"):
@@ -8,11 +11,23 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template ("home.html")
+    posts = list(mongo.db.blogs.find())
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = 'About')
 
 
 if __name__ == "__main__":
