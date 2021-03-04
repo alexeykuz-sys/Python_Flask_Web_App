3fd142fa0a9ddbbb9bbf97aa7a06f0af7d1e8652
unknown <alexey kuzmis>
2021-03-04 14:23:44 +0000

    init commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import  os
+import flask from Flask
+
+
+if os.path.exist("env.py")
+    import env
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "dfdfgfghg"
+
+
+if __name__ = "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
